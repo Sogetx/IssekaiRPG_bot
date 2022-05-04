6debf77627cbb10a5acc457dd8c3ccb3e83eac37
Sogetx <levstepanenko@gmail.com>
2022-05-04 22:33:59 +0300

    Мелкие дополнения

--- Events/Church.py
+++ Events/Church.py
@@ -1,13 +1,19 @@
 from Events.Event import Event
-
+import random
 
 class Church(Event):
     def __init__(self):
         super().__init__()
         self.description = "Вы заходите в небольшое святилище, оно аккуратное, чистое хотя никого в нем нету. " \
-                           "Постояв у алтаря вам становиться легче а раны затягиваються"
+
 
     def action(self, user):
-        result = user.max_hp - user.hp
-        user.heal(user.max_hp)
-        return "+" + str(result) + " ❤"
+        val = random.randint(1, 2)
+        if val == 1:
+            result = user.max_hp - user.hp
+            user.heal(user.max_hp)
+            return "Постояв у алтаря вам становиться легче а раны затягиваються.\n+" + str(result) + " ❤"
+        elif val == 2:
+            return "Вы думали что боги смилуються над вами?\nМожете раслабиться вы им все также безраличны как и раньше."
+        else:
+            return 0 # заглушка для вставки битвы с мобов в будующем
